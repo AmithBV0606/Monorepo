fe165be5f0c2232fc30e312d1659fa169463e50d
Amith B V <amithrao0606@gmail.com>
2025-09-06 18:15:03 +0530

    Added a new admin page and created an "Admin" component.

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import Admin from "@repo/ui/admin";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <h1>Hello, Admin!!</h1>
+      <Admin />
+    </div>
+  );
+}
