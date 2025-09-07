6f1f15e965d97b9bf89937caaca996f5ffba2e64
Amith B V <amithrao0606@gmail.com>
2025-09-07 12:23:24 +0530

    Added new common folder in package folder.

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import { BACKEND_URL } from "@repo/common/config";
 
 const app = express();
 const PORT = 3003;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express server!!");
+  console.log(BACKEND_URL);
+  res.json({ message: "Hello from Express server!!", URL: BACKEND_URL });
 });
 
 app.listen(PORT, () => {
---
+++ packages/common/src/index.ts
@@ -0,0 +1,3 @@
+export const VALUE = "Amith B V";
+
+export const BACKEND_URL = "https://api.google.com/";
